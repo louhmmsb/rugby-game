672a830461206be7c57308f4fb1adbdc401ae9e8
Lourenço Bogo <louhmmsb@hotmail.com>
2022-03-27 14:26:04 -0300

    src/defender.c: Added the logic for the Defender
    Added the logic for the Defender on the file defender.c. It goes
    forward once, then keeps moving up and down until the game finishes.

--- src/defender.c
+++ src/defender.c
@@ -19,11 +19,31 @@
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
   UNUSED(attacker_spy);
 
   // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  static position_t defender_old_position = INVALID_POSITION;
+  static unsigned char first_turn = 0;
+  static int direction_y = 1;
+  direction_t next_direction = (direction_t) DIR_LEFT;
+
+  if (!first_turn) {
+    first_turn = 1;
+  }
+
+  else if (equal_positions(defender_position, defender_old_position)){
+    direction_y *= -1;
+    next_direction = (direction_t) {direction_y, 0};
+  }
+
+  else {
+    next_direction = (direction_t) {direction_y, 0};
+  }
+  
+  printf("defender_move == {%d, 0}\n", direction_y);
+  defender_old_position = defender_position;
+  return next_direction;
+
 }
 
 /*----------------------------------------------------------------------------*/
